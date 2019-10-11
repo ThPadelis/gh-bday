0ffd904a5dad70abc6387c1c09189b14f89b7fd6
ThPadelis <padelis.theodosiou@gmail.com>
2019-10-11 21:40:20 +0300

    Bootstrap app

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <birthday></birthday>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {
+    Birthday: () =>
+      import(
+        /* webpackChunkName: "birthday-component" */ "@/components/Birthday.vue"
+      )
+  }
+};
+</script>
---
+++ src/components/Birthday.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>GitHub Birthday</h1>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

199f0f1358cd2ca166eb90cbe4ef7127ee3b129c
ThPadelis <padelis.theodosiou@gmail.com>
2019-10-13 19:25:54 +0300

    Updated components

--- src/components/Birthday.vue
+++ src/components/Birthday.vue
@@ -1,135 +1,85 @@
 <template>
-  <main class="min-vh-100 d-flex align-items-center">
+  <section class="min-vh-100 d-flex justify-content-center align-items-center">
     <div class="container">
+      <!-- Logo -->
       <div class="row">
         <div class="col-12 text-center">
-          <img :src="require('@/assets/steroidtocat.png')" alt="Octocat icon" height="250" />
+          <img :src="require('@/assets/steroidtocat.png')" alt="octocat on steroid" height="200" />
         </div>
       </div>
+      <!-- Input -->
       <div class="row justify-content-center">
-        <div class="col-5">
-          <!-- <div class="form-group">
+        <div class="col-12 col-md-3 text-center">
+          <div class="input-group border rounded pl-3">
             <input
               type="text"
               name="username"
               id="username"
-              v-model="username"
               class="form-control"
-              placeholder="@username"
-              autocomplete="off"
-              @keyup.enter="onSubmit()"
-            />
-          </div>-->
-          <div class="input-group">
-            <div class="input-group-prepend border-bottom">
-              <span class="input-group-text">
-                <i class="fa fa-at"></i>
-              </span>
-            </div>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              v-model="username"
-              class="form-control"
+              v-model.trim="username"
               placeholder="GitHub username"
-              autocomplete="off"
-              @keyup.enter="onSubmit()"
+              @keyup.enter="getUser()"
             />
             <div class="input-group-append">
-              <button
-                class="btn shadow-none border-bottom btn-outline-secondary"
-                @click="onSubmit()"
-                type="button"
-              >
+              <button class="btn btn-secondary" @click="getUser()">
                 <span class="fa fa-search"></span>
               </button>
             </div>
           </div>
         </div>
-        <div class="col-12" v-if="user">
-          <div class="d-flex align-items-center justify-content-center">
-            <img :src="user.avatar_url" alt="user avatar" height="75" class="rounded-circle mr-4" />
-            <div class="mb-0">
-              <h4>
-                <a
-                  :href="user.html_url"
-                  target="_blank"
-                  class="text-dark mr-1"
-                >{{user.name || username | capitalize}}</a>
-                <span class="mr-1">joied GitHub on</span>
-                <span class="d-block text-underline">{{user.created_at | moment}}</span>
-              </h4>
-            </div>
+      </div>
+      <!-- Response -->
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <div v-if="!userNotFound">
+            <user :user="user"></user>
           </div>
+          <div v-else>Not found</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <pre>{{user}}</pre>
         </div>
       </div>
     </div>
-  </main>
+  </section>
 </template>
 
 <script>
 import axios from "axios";
-import * as moment from "moment";
 export default {
   name: "birthday",
-  filters: {
-    capitalize(value) {
-      if (!value) return "";
-      value = value.toString();
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    },
-    moment(date, format = "LL") {
-      return moment(date).format(format);
-    }
+  components: {
+    User: () =>
+      import(/* webpckChunkName: "user-component" */ "@/components/User.vue")
   },
   data() {
     return {
       username: "",
-      user: null
+      user: null,
+      userNotFound: true
     };
   },
   methods: {
-    getUserInfo(username) {
-      return axios
-        .get(`https://api.github.com/users/${username}`)
-        .then(response => response.data)
-        .catch(error => {
+    async getUser() {
+      try {
+        const response = await axios.get(
+          `https://api.github.com/users/${this.username}`
+        );
+        this.userNotFound = false;
+        this.user = response.data;
+      } catch (error) {
+        if (error.response.status === 404) {
+          this.userNotFound = true;
           this.user = null;
-        });
-    },
-    onSubmit() {
-      if (this.username.trim() !== "") {
-        this.getUserInfo(this.username).then(user => {
-          this.user = user;
-          this.getNextBday(new Date(user.created_at));
-        });
+        }
       }
-    },
-    getNextBday(birthday) {
-      let today = moment();
-      var bday = moment(
-        new Date(today.get("year"), birthday.getMonth(), birthday.getDate())
-      );
-
-      let nextBday = moment.duration(bday.diff(moment())).asDays();
-      console.log(Math.abs(Math.floor(nextBday)));
     }
   }
 };
 </script>
 
-<style scoped>
-main {
-  background: #f8f9fa;
-}
-
-[class*="col-"] {
-  margin-top: 1.5rem;
-  margin-bottom: 1.5rem;
-}
-
-.text-underline {
-  text-decoration: underline;
-}
+<style>
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center" v-if="user">
+    <img :src="user.avatar_url" alt="user avatar" class="rounded-circle mr-3" height="150" />
+    <h2 class="mb-0">
+      <a :href="user.html_url" class="text-dark">{{getName}}</a>
+      <span class="px-1">joined GitHub on</span>
+      <span class="d-block">{{user.created_at | date}}</span>
+    </h2>
+  </div>
+</template>
+
+<script>
+import * as moment from "moment";
+export default {
+  name: "user",
+  props: {
+    user: {
+      required: true
+    }
+  },
+  filters: {
+    date(date, format = "LL") {
+      return moment(date).format(format);
+    }
+  },
+  computed: {
+    getName() {
+      return this.user.name !== "" ? this.user.name : this.user.login;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
